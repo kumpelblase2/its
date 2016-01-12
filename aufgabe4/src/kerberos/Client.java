4e327bd1135eefbada7aaaf981eac2c26a5fda0f
Tim Hagemann <tim@infinityblade.de>
2016-01-12 09:48:24 +0100

    Added init files for last exercise

---
+++ aufgabe4/src/kerberos/Client.java
@@ -0,0 +1,49 @@
+package kerberos;
+
+/* Simulation einer Kerberos-Session mit Zugriff auf einen Fileserver
+ /* Client-Klasse
+ */
+
+import java.util.*;
+
+public class Client extends Object {
+
+	private KDC myKDC; // Konstruktor-Parameter
+
+	private String currentUser; // Speicherung bei Login nötig
+	private Ticket tgsTicket = null; // Speicherung bei Login nötig
+	private long tgsSessionKey; // K(C,TGS) // Speicherung bei Login nötig
+
+	// Konstruktor
+	public Client(KDC kdc) {
+		myKDC = kdc;
+	}
+
+	public boolean login(String userName, char[] password) {
+		/* ToDo */
+	}
+
+	public boolean showFile(Server fileServer, String filePath) {
+		/* ToDo */
+	}
+
+	/* *********** Hilfsmethoden **************************** */
+
+	private long generateSimpleKeyFromPassword(char[] passwd) {
+		// Liefert einen eindeutig aus dem Passwort abgeleiteten Schlüssel
+		// zurück, hier simuliert als long-Wert
+		long pwKey = 0;
+		if (passwd != null) {
+			for (int i = 0; i < passwd.length; i++) {
+				pwKey = pwKey + passwd[i];
+			}
+		}
+		return pwKey;
+	}
+
+	private long generateNonce() {
+		// Liefert einen neuen Zufallswert
+		long rand = (long) (100000000 * Math.random());
+		return rand;
+	}
+}
---
+++ aufgabe4/src/kerberos/Main.java
@@ -0,0 +1,7 @@
+package kerberos;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
